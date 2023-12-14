a5190e3b2c9a6d21dca63d55b20e0eb1ec54c736
Tamás Pálya <tamas.palya01@gmail.com>
2023-12-14 20:37:09 +0100

    Set up Order.API Program.cs

--- Order.API/Program.cs
+++ Order.API/Program.cs
@@ -1,3 +1,14 @@
+using EventBus.Messages.Common;
+using MassTransit;
+using Order.API.EventBusConsumer;
+using Microsoft.OpenApi.Models;
+using Order.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using HealthChecks.UI.Client;
+using Logging;
+using Serilog;
+using MediatR;
+using Order.Application.Features.Orders.Commands.CheckoutOrder;
 
 namespace Order.API
 {
@@ -8,25 +19,78 @@ namespace Order.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddMassTransit(x =>
+            {
+                x.AddConsumer<BasketCheckoutConsumer>();
+                
+                x.UsingRabbitMq((ctx, cfg) =>
+                {
+                    cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+                    // Additional configuration for RabbitMQ can be done here
+
+                    cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c => {
+                        c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+                    });
+                });
+            });
+
+            builder.Services.AddScoped<BasketCheckoutConsumer>();
+            builder.Services.AddAutoMapper(typeof(Program));
+
+            // Configure Serilog
+            builder.Host.UseSerilog(SeriLogger.Configure);
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Order.API", Version = "v1" });
+            });
+
+            builder.Services.AddHealthChecks()
+                            .AddDbContextCheck<OrderContext>();
 
             var app = builder.Build();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+            });
+
+            // Migrate and seed the database
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<OrderContext>();
+                    var logger = services.GetService<ILogger<OrderContextSeed>>();
+                    OrderContextSeed.SeedAsync(context, logger).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating or initializing the database.");
+                }
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order.API v1"));
             }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
