fa87882f83c2f3f7ea9749e0133a71e0a6aaa906
tamaspalya <tamas.palya01@gmail.com>
2023-12-18 12:09:13 +0100

    Update book management

--- BookManagement.API/Controllers/BookManagementController.cs
+++ BookManagement.API/Controllers/BookManagementController.cs
@@ -1,1 +1,37 @@
-﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BookManagement.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BookManagementController : ControllerBase
+{
+    private readonly ILogger<BookManagementController> _logger;
+
+    public BookManagementController(ILogger<BookManagementController> logger)
+    {
+        _logger = logger;
+    }
+
+    // This method can be used to add a new book
+    [HttpPost(Name = "AddBook")]
+    public IActionResult AddBook()
+    {
+        throw new NotImplementedException("This method is not implemented yet.");
+    }
+
+    // This method can be used to update an existing book
+    [HttpPut(Name = "UpdateBook")]
+    public IActionResult UpdateBook()
+    {
+        throw new NotImplementedException("This method is not implemented yet.");
+    }
+
+    // This method can be used to remove an existing book
+    [HttpDelete(Name = "RemoveBook")]
+    public IActionResult RemoveBook()
+    {
+        throw new NotImplementedException("This method is not implemented yet.");
+    }
+}
